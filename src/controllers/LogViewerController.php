e00b76da33239b90399ad91edef839bc29f9969f
atlas-wong <atlas-wong@users.noreply.github.com>
2018-01-19 21:41:46 +0800

    Update LogViewerController.php

--- src/controllers/LogViewerController.php
+++ src/controllers/LogViewerController.php
@@ -1,5 +1,5 @@
 <?php
-namespace Rap2hpoutre\LaravelLogViewer;
+namespace AtlasWong\LaravelLogViewer;
 
 if (class_exists("\\Illuminate\\Routing\\Controller")) {
     class BaseController extends \Illuminate\Routing\Controller {}
@@ -7,36 +7,33 @@ if (class_exists("\\Illuminate\\Routing\\Controller")) {
     class BaseController extends \Laravel\Lumen\Routing\Controller {}
 }
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Response;
+
 class LogViewerController extends BaseController
 {
-    protected $request;
-
-    public function __construct ()
-    {
-        $this->request = app('request');
-    }
 
     public function index()
     {
 
-        if ($this->request->input('l')) {
-            LaravelLogViewer::setFile(base64_decode($this->request->input('l')));
+        if (Request::input('l')) {
+            LaravelLogViewer::setFile(base64_decode(Request::input('l')));
         }
 
-        if ($this->request->input('dl')) {
-            return $this->download(LaravelLogViewer::pathToLogFile(base64_decode($this->request->input('dl'))));
-        } elseif ($this->request->has('del')) {
-            app('files')->delete(LaravelLogViewer::pathToLogFile(base64_decode($this->request->input('del'))));
-            return $this->redirect($this->request->url());
-        } elseif ($this->request->has('delall')) {
-            foreach(LaravelLogViewer::getFiles(true) as $file){
-                app('files')->delete(LaravelLogViewer::pathToLogFile($file));
-            }
-            return $this->redirect($this->request->url());
+        if (Request::input('dl')) {
+            return Response::download(LaravelLogViewer::pathToLogFile(base64_decode(Request::input('dl'))));
+        } elseif (Request::has('del')) {
+            File::delete(LaravelLogViewer::pathToLogFile(base64_decode(Request::input('del'))));
+            return $this->redirect(Request::url());
         }
 
-        return app('view')->make('laravel-log-viewer::log', [
-            'logs' => LaravelLogViewer::all(),
+        $logs = LaravelLogViewer::all();
+
+        return View::make('laravel-log-viewer::log', [
+            'logs' => $logs,
             'files' => LaravelLogViewer::getFiles(true),
             'current_file' => LaravelLogViewer::getFileName(),
             'config' => LaravelLogViewer::getConfigs(),
@@ -49,16 +46,6 @@ class LogViewerController extends BaseController
             return redirect($to);
         }
 
-        return app('redirect')->to($to);
-    }
-
-    private function download($data)
-    {
-        if (function_exists('response')) {
-            return response()->download($data);
-        }
-
-        // For laravel 4.2
-        return app('\Illuminate\Support\Facades\Response')->download($data);
+        return Redirect::to($to);
     }
 }
